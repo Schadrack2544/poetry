b0f6ddb4c075de97ecb1ea6fb9eebff918f4a663
Schadrack2544 <nzayinaschad250@gmail.com>
2023-04-12 20:13:29 +0200

    changing logo icons.

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,23 @@
   const trimmedText = text.substring(0, maxLength) + (text.length > maxLength ? '...' : '');
   document.querySelector('.miniText').textContent = trimmedText;
 
+  // get the header element
+const header = document.querySelector('menu');
+
+// get the offset top of the header element
+const headerOffsetTop = header.offsetTop;
+
+// add an event listener to the window object to listen for scroll events
+window.addEventListener('scroll', function() {
+  // if the window has been scrolled past the offset top of the header element,
+  // add the 'fixed' class to the header element; otherwise, remove it
+  if (window.pageYOffset >= headerOffsetTop) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
+});
+
   $(document).ready(function() {
     var currentIndex = 0;
     var images = $('.featured-image img');
